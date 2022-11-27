a7c33160ab30c4ec257d329bd5e9d04d2ff6d4d4
Flores-Oz <100817423+Flores-Oz@users.noreply.github.com>
2022-11-27 02:44:35 -0600

    Calculadora

---
+++ js/script.js
@@ -0,0 +1,52 @@
+window.addEventListener('load', () => {   
+    const keypadButtons = document.getElementsByClassName('keypad-button');
+    const display = document.querySelector('.calculator-display');
+
+    const keypadButtonsArray = Array.from(keypadButtons);
+
+    keypadButtonsArray.forEach( (button) => {
+
+        button.addEventListener('click', () => {
+            /* Llamo a una funcion cada vez que ocurra un click */
+            calculadora(button, display);
+        });
+
+    });
+});
+
+function calculadora(button, display) {
+    /* funcion que decide que hacer */
+    switch (button.innerHTML) {
+        case 'C':
+            borrar(display);
+            break;
+
+        case '=':
+            calcular(display);
+            break;
+    
+        default:
+            actualizar(display, button);
+            break;
+    }
+
+}
+
+function calcular(display) {
+    display.innerHTML = eval(display.innerHTML);
+}
+
+function actualizar(display, button) {
+    
+    if(display.innerHTML == 0){
+        display.innerHTML = '';
+    }
+
+    display.innerHTML += button.innerHTML;
+}
+
+function borrar(display) {
+    if(display.innerHTML != 0) {
+        display.innerHTML = 0;
+    }
+}
